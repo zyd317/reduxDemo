931ebd4ac96f327664efc445a94c25b6f03fe6ce
yidi.zhao <yidi.zhao@qunar.com>
2017-12-04 15:11:36 +0800

    增加refs的使用方式

--- common/reducers/counter.js
+++ common/reducers/counter.js
@@ -4,6 +4,9 @@
  * 同一个reducer应该都是操作同一个变量
  *
  * 传入的每个action都是一个对象，都至少有一个type属性，来根据type执行不同的操作，得到不同的value
+ *
+ * default的必须要return state; 默认值，
+ * 因为所有的state都在一个store里面，dispatch的时候会遍历所有的reducer，不符合的都会走到default里面
  */
 import { INCREMENT_COUNTER, DECREMENT_COUNTER, MULTIPLY_COUNTER } from '../constants/counter1'
 
