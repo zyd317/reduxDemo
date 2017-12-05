bc0b5d3c8b169cd8e0e7829229f34858eed83acb
yidi.zhao <yidi.zhao@qunar.com>
2017-12-05 15:14:03 +0800

    修改文案

--- common/components/Counter/Counter.js
+++ common/components/Counter/Counter.js
@@ -11,7 +11,7 @@ const Counter = ({counterState, increment, decrement, incrementIfOdd, incrementA
     点击了: {counterState} 次
     <button className="btn-default" onClick={increment}>+</button>
     <button className="btn-default" onClick={decrement}>-</button>
-    <button className="btn-default" onClick={incrementIfOdd}>如果是偶数就增加</button>
+    <button className="btn-default" onClick={incrementIfOdd}>如果是奇数就增加</button>
     <button className="btn-default" onClick={() => incrementAsync()}>异步增加</button>
     <button className="btn-default" onClick={() => multiply(3)}>乘以n</button>
   </p>
